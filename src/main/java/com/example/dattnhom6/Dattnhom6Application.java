2c362ecc7e8d24066c75428cd46c860bcc0df527
datntph25535 <datntph25535@fpt.edu.vn>
2023-09-27 23:22:31 +0700

    Initial commit

---
+++ src/main/java/com/example/dattnhom6/Dattnhom6Application.java
@@ -0,0 +1,13 @@
+package com.example.dattnhom6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dattnhom6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dattnhom6Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dattnhom6/Dattnhom6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dattnhom6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dattnhom6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
